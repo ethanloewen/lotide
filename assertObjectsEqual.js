15c914650e0b76520b9889f3ee66669bcf181de2
Ethan Loewen <ethanloewen@gmail.com>
2022-02-10 01:17:27 +0000

    Created a function to help test functions that return objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,70 @@
+//Function that checks if 2 arrays are equal
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// Checks if two objects are equal
+const eqObjects = function(object1, object2) {
+
+  const objectOneKeys = Object.keys(object1);
+  const objectTwoKeys = Object.keys(object2);
+
+  if (objectOneKeys.length !== objectTwoKeys.length) {
+    return false;
+  }
+
+  for (const key of objectOneKeys) {
+
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key]) === false) {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+// helps test functions that return objects
+const assertObjectsEqual = function(actual, expected) {
+  // import util library
+  const inspect = require('util').inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertation Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const obj1 = {
+  key1: '1',
+  key2: '2',
+  key3: '3'
+};
+
+const obj2 = {
+  key1: '3',
+  key2: '2',
+  key3: '1'
+};
+
+assertObjectsEqual(obj1, obj1);
+assertObjectsEqual(obj1, obj2);
+
