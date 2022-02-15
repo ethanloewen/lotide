7c83aaf3883810f3f21a847acacc78d83d43c76f
Ethan Loewen <ethanloewen@gmail.com>
2022-02-15 00:47:57 +0000

    changed spacing in countLettersTest to match other test files

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -2,7 +2,9 @@ const countLetters = require('../countLetters');
 const { assert } = require('chai');
 
 describe('#countLetters', () => {
+
   it('returns the count of each letter', () => {
     assert.deepEqual(countLetters('test'), { t: 2, e: 1, s: 1 });
   });
+  
 });
